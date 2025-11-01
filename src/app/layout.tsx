0563a849f3bb71ef51ebf039a640d7d7bf8dbebf
momo0817 <furumomo0817@gmail.com>
2025-11-01 18:25:22 +0900

    remove meta-data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import { Geist, Geist_Mono, Noto_Serif, PT_Serif } from "next/font/google";
 import "./globals.css";
 import { aboutMe } from "@/data/aboutme";
-import { customMetadata } from "@/data/title-description";
+// import { customMetadata } from "@/data/title-description";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
